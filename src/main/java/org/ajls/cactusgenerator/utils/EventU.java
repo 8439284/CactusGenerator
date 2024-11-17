c4dc5d4454026f94e04cae4ab54aae83226703d7
Your Name <you@example.com>
2024-11-17 15:20:35 +0800

    Bloody Update

---
+++ src/main/java/org/ajls/cactusgenerator/utils/EventU.java
@@ -0,0 +1,66 @@
+package org.ajls.cactusgenerator.utils;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class EventU {
+    public static double getFinalHealth(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+        double finalDamage = event.getFinalDamage();
+        if (entity instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity) entity;
+            double livingEntityHealthBefore = livingEntity.getHealth();
+            double livingEntityAbsorptionBefore = livingEntity.getAbsorptionAmount();
+            double livingEntityAbsorptionAfter;
+            double livingEntityHealthAfter;// = livingEntity.getHealth() - finalDamage;
+            if (livingEntityAbsorptionBefore >= finalDamage) {
+                livingEntityAbsorptionAfter = livingEntityAbsorptionBefore - finalDamage;
+                return livingEntityHealthBefore;
+            }
+            else {
+                livingEntityAbsorptionAfter = 0;
+                livingEntityHealthAfter = livingEntityHealthBefore + livingEntityAbsorptionBefore - finalDamage;
+                if (livingEntityAbsorptionAfter <= 0) return 0;
+                return livingEntityHealthAfter;
+            }
+
+//            return livingEntityHealthAfter;
+        }
+        else {
+            return -1;
+        }
+
+
+    }
+
+    public static double getFinalDamage(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+        double finalDamage = event.getFinalDamage();
+        if (entity instanceof LivingEntity) {
+            LivingEntity livingEntity = (LivingEntity) entity;
+            double livingEntityHealthBefore = livingEntity.getHealth();
+            double livingEntityAbsorptionBefore = livingEntity.getAbsorptionAmount();
+            double livingEntityHPBefore = livingEntityHealthBefore + livingEntityAbsorptionBefore;
+            if (livingEntityHPBefore >= finalDamage) {
+                return finalDamage;
+            }
+            else {
+                return livingEntityHPBefore;
+            }
+//            finalDamage -= livingEntityAbsorptionBefore;
+//            finalDamage -= livingEntityHealthBefore;
+//            if (finalDamage <= 0) return 0;
+
+//            return livingEntityHealthAfter;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public static double getFinalDamage(EntityDamageByEntityEvent event) {
+        return getFinalDamage((EntityDamageEvent) event);
+    }
+}
